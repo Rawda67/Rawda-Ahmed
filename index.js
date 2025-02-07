a8b720b39e7917d8bc13c0246d3a066d56678597
Rawda Ahmed <158529405+Rawda67@users.noreply.github.com>
2025-02-07 05:36:24 +0200

    portfolio

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
+function navigateTo(url) {
+    window.location.href = url;
+}
+
 function downloadCertification() {
-    window.location.href = "https://drive.google.com/file/d/1m5kShOARH5iTlL8ECw1NASwgBkPeXcVj/view?usp=drivesdk";
+    navigateTo("https://drive.google.com/file/d/1m5kShOARH5iTlL8ECw1NASwgBkPeXcVj/view?usp=drivesdk");
 }
+
 function viewGDGInfo() {
-    window.location.href = "https://www.linkedin.com/company/gdgoncampus-benhauniversity/";
+    navigateTo("https://www.linkedin.com/company/gdgoncampus-benhauniversity/");
 }
+
 function viewSourceCodeP1() {
-    window.location.href = "https://github.com/Rawda67/Programming-Academy-";
+    navigateTo("https://github.com/Rawda67/Programming-Academy-");
 }
+
 function viewProject1() {
-    window.location.href = "https://drive.google.com/drive/folders/1mKhUsMzE1xwFYF2VdkwoMdP2ldgnAnxR";
-    }
+    navigateTo("https://drive.google.com/drive/folders/1mKhUsMzE1xwFYF2VdkwoMdP2ldgnAnxR");
+}
+
 function viewProject2() {
-        window.location.href = "https://drive.google.com/drive/folders/1n6YEezxy1Jw846uzhxd3fhyLGJjCHLEK";
-        }
+    navigateTo("https://drive.google.com/drive/folders/1n6YEezxy1Jw846uzhxd3fhyLGJjCHLEK");
+}
+
 function viewSourceCodeP2() {
-            window.location.href = " https://github.com/Rawda67/Koala-s-Lovers"
-        }
-        function viewProject3() {
-            window.location.href = "https://drive.google.com/drive/folders/1nNZOoD4d6742XH1qrfNjm1SRjRiX0K_L";
-            }
-         function viewSourceCodeP3() {
-                window.location.href = "https://github.com/Rawda67/Text-Me"
-            }
-            function viewSourceCodeP4() {
-                window.location.href = "  https://github.com/Rawda67/Hospital-DS"
-            }
+    navigateTo("https://github.com/Rawda67/Koala-s-Lovers");
+}
+
+function viewProject3() {
+    navigateTo("https://drive.google.com/drive/folders/1nNZOoD4d6742XH1qrfNjm1SRjRiX0K_L");
+}
+
+function viewSourceCodeP3() {
+    navigateTo("https://github.com/Rawda67/Text-Me");
+}
+
+function viewSourceCodeP4() {
+    navigateTo("https://github.com/Rawda67/Hospital-DS");
+}
+
 function viewResume() {
-    window.location.href = "https://drive.google.com/file/d/1nj6DjNuy3Q8ncW4XU1pogUMSvQZYM9ES/view?usp=drivesdk";
+    navigateTo("https://drive.google.com/file/d/1nj6DjNuy3Q8ncW4XU1pogUMSvQZYM9ES/view?usp=drivesdk");
 }
